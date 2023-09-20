583c713eb7f48a2813a4e4a3cd24f409e1fd8218
Prince Narteh <prinarteh@gmail.com>
2023-09-20 13:49:30 +0000

    feat: add layout

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./components/layout";
 import { DashBoard } from "./pages/dashboard";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<DashBoard />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<DashBoard />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
