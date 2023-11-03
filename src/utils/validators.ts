1d2be8dc6cde413e81bcb99ce8aefa5bc842a2ac
Prince Narteh <prinarteh@gmail.com>
2023-11-03 03:39:20 +0000

    minor changes

--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -1,4 +1,4 @@
-import { Admin } from "@custom-types/index";
+import { Admin, DeliveryCompany } from "@custom-types/index";
 import { z } from "zod";
 
 export const adminResolver = (admin: Admin | undefined) => {
@@ -71,4 +71,69 @@ export const adminResolver = (admin: Admin | undefined) => {
       });
     }
   });
+};
+const MAX_FILE_SIZE = 500000;
+const ACCEPTED_IMAGE_TYPES = [
+  "image/jpeg",
+  "image/jpg",
+  "image/png",
+  "image/webp",
+];
+export const deliveryCompanyResolver = (
+  deliveryCompany: DeliveryCompany | undefined
+) => {
+  const schema = z.object({
+    name: z
+      .string({ required_error: "First name is required" })
+      .min(1, "First name is required"),
+    email: z.string().email("Please enter a valid email"),
+    phone_number: z
+      .string({ required_error: "Phone number is required" })
+      .min(10, "Please enter a valid phone number")
+      .max(13, "Please enter a valid phone number"),
+    alternate_phone_number: z.union([
+      z
+        .string({ required_error: "Phone number is required" })
+        .min(10, "Please enter a valid phone number")
+        .max(13, "Please enter a valid phone number"),
+      z.undefined(),
+    ]),
+    whatsapp_number: z
+      .string({ required_error: "Whatsapp number is required" })
+      .min(10, "Please enter a valid phone number")
+      .max(13, "Please enter a valid phone number"),
+    location: z
+      .string({ required_error: "Location is required" })
+      .min(1, "Location is required"),
+    ownerFirstName: z
+      .string({ required_error: "Owner's first name is required" })
+      .min(1, "Owner's first name is required"),
+    ownerLastName: z
+      .string({ required_error: "Owner's last name is required" })
+      .min(1, "Owner's last name is required"),
+    ownerPhoneNumber: z
+      .string({ required_error: "Owner's phone number is required" })
+      .min(1, "Owner's phone number is required"),
+    slide_images: z
+      .any()
+      .refine((file) => file?.size <= MAX_FILE_SIZE, `Max image size is 5MB.`)
+      .refine(
+        (file) => ACCEPTED_IMAGE_TYPES.includes(file?.type),
+        "Only .jpg, .jpeg, .png and .webp formats are supported."
+      ),
+  });
+
+  if (!deliveryCompany) {
+    return schema
+      .extend({
+        password: z.string().min(6, "Passwords must be at least 6 characters"),
+        confirmPassword: z.string().min(6, "Please confirm your password"),
+      })
+      .refine((data) => data.password === data.confirmPassword, {
+        message: "Passwords don't match",
+        path: ["confirmPassword"], // path of error
+      });
+  }
+
+  return schema;
 };
