1e70d0eaa2a54661027b517ac1cabf9ec2e56e03
Prince Narteh <prinarteh@gmail.com>
2023-12-15 15:07:29 +0000

    update packages

--- src/components/sidebar/SidebarData.tsx
+++ src/components/sidebar/SidebarData.tsx
@@ -22,61 +22,21 @@ export const sidebarData: SidebarData[] = [
         groupTitle: "Administrators",
         icon: <Icon icon="clarity:administrator-line" fontSize={"22"} />,
         path: "/admins",
-        subNav: [
-          {
-            title: "All Administrators",
-            path: "/admins",
-          },
-          {
-            title: "Add Administrators",
-            path: "/admins/add-admin",
-          },
-        ],
       },
       {
         groupTitle: "Delivery Companies",
         icon: <Icon icon="carbon:delivery" fontSize={"22"} />,
         path: "/delivery-companies",
-        subNav: [
-          {
-            title: "All Delivery Companies",
-            path: "/delivery-companies",
-          },
-          {
-            title: "Add Delivery Company",
-            path: "/delivery-companies/add-delivery-company",
-          },
-        ],
       },
       {
         groupTitle: "Shops",
         icon: <Icon icon="icon-park-outline:shop" fontSize={"22"} />,
         path: "/shops",
-        subNav: [
-          {
-            title: "All Shops",
-            path: "/shops",
-          },
-          {
-            title: "Add Shop",
-            path: "/shops/add-shop",
-          },
-        ],
       },
       {
         groupTitle: "Users",
         icon: <Icon icon="ph:users-three-bold" fontSize={"22"} />,
         path: "/users",
-        subNav: [
-          {
-            title: "All Users",
-            path: "/users",
-          },
-          {
-            title: "Add User",
-            path: "/users/add-user",
-          },
-        ],
       },
       {
         groupTitle: "Roles and Permissions",
@@ -94,16 +54,6 @@ export const sidebarData: SidebarData[] = [
         groupTitle: "Products",
         icon: <Icon icon="icon-park-outline:ad-product" className="h-6 w-6" />,
         path: "/products",
-        subNav: [
-          {
-            title: "App Products",
-            path: "/products",
-          },
-          {
-            title: "Add Product",
-            path: "/products/add-product",
-          },
-        ],
       },
       {
         groupTitle: "Orders",
--- src/components/sidebar/SubMenu.tsx
+++ src/components/sidebar/SubMenu.tsx
@@ -19,7 +19,7 @@ function SubMenu({ item }: { item: SidebarData }) {
           <div className={`flex justify-between items-center`}>
             <div
               className={`sidebarLink w-full space-y-4 flex text-black font-bold justify-between items-center list-none h-12 font-second text-md mx-4 cursor-pointer rounded-lg  ${
-                pathname.includes(nav.path) ? "bg-white text-primary" : ""
+                pathname === nav.path ? "bg-white text-primary" : ""
               }`}
             >
               <Link
@@ -29,18 +29,14 @@ function SubMenu({ item }: { item: SidebarData }) {
                 <div className="flex items-center">
                   <div
                     className={`mr-2 mb-1 ${
-                      pathname.includes(nav.path)
-                        ? "text-primary"
-                        : "text-white"
+                      pathname === nav.path ? "text-primary" : "text-white"
                     }`}
                   >
                     {nav.icon}
                   </div>
                   <div
                     className={`font-ray SidebarLabel font-semibold line-clamp-1 ${
-                      pathname.includes(nav.path)
-                        ? "text-primary"
-                        : "text-white"
+                      pathname === nav.path ? "text-primary" : "text-white"
                     }`}
                   >
                     {nav.groupTitle}
@@ -48,7 +44,7 @@ function SubMenu({ item }: { item: SidebarData }) {
                 </div>
                 <div
                   className={`${
-                    pathname.includes(nav.path) ? "text-primary" : "text-white"
+                    pathname === nav.path ? "text-primary" : "text-white"
                   }`}
                 >
                   {nav.subNav && nav.subNav.length > 0 && (
@@ -62,46 +58,7 @@ function SubMenu({ item }: { item: SidebarData }) {
                 </div>
               </Link>
             </div>
-
-            <div
-              className={`${
-                pathname.includes(nav.path)
-                  ? "w-1 mr-[0.10rem] h-9 bg-white rounded-full"
-                  : ""
-              } `}
-            ></div>
           </div>
-
-          {pathname.includes(nav.path) &&
-            nav.subNav &&
-            nav.subNav.length > 0 &&
-            nav.subNav.map((subNavItem, index) => (
-              <Link
-                key={index}
-                to={subNavItem.path}
-                className={`pl-10 flex py-2 items-center tracking-wider font-ray font-semibold duration-300`}
-              >
-                {pathname === subNavItem.path ? (
-                  <Icon
-                    icon="game-icons:check-mark"
-                    className={`w-3 mr-2 mt-1 text-white`}
-                  />
-                ) : (
-                  <Icon
-                    icon="ci:dot-03-m"
-                    className={`w-3 mr-2 mt-1 text-white`}
-                    fontSize={20}
-                  />
-                )}
-                <span
-                  className={`${
-                    pathname.includes(nav.path) ? "text-white" : "text-primary"
-                  } flex items-center`}
-                >
-                  {subNavItem.title}
-                </span>
-              </Link>
-            ))}
         </React.Fragment>
       ))}
     </div>
